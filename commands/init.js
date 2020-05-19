d1ff9bd063ed5e6ab5345e1ca670de2f3b65b90d
Stephan Arenswald <stephan@sarensw.com>
2020-05-19 07:04:55 +0200

    Added welcome message

--- commands/init.js
+++ commands/init.js
@@ -77,6 +77,34 @@ const init = async () => {
         }
       }
     }
+
+    // ----------------
+    // Print welcome info
+    // ----------------
+
+    console.log()
+    console.log('Welcome to')
+    console.log(' ______         _                  _   _ _       _')
+    console.log('|  ____|       | |                | \\ | (_)     (_)')
+    console.log('| |__ ___  __ _| |_ _   _ _ __ ___|  \\| |_ _ __  _  __ _ ___')
+    console.log('|  __/ _ \\/ _` | __| | | | \'__/ _ \\ . ` | | \'_ \\| |/ _` / __|')
+    console.log('| | |  __/ (_| | |_| |_| | | |  __/ |\\  | | | | | | (_| \\__ \\')
+    console.log('|_|  \\___|\\__,_|\\__|\\__,_|_|  \\___|_| \\_|_|_| |_| |\\__,_|___/')
+    console.log('                                               _/ |')
+    console.log('                                              |__/           ')
+    console.log('')
+    console.log('You are now on the ' + chalk.black.bgGreen('STARTER PLAN'))
+    console.log('Check out ' + chalk.blue.underline('https://featureninjas.com') + ' for more info on the PRO plan which includes rollout strategies and extended logic')
+    console.log('')
+    console.log(chalk.blue('Next Steps:'))
+    console.log(' 1) Add or updated flags in flags.json5')
+    console.log(' 2) Push changes to your repository using git add/commit/push')
+    console.log(' 3) Use our API to retrieve your flags')
+    console.log('    Example: curl -X GET \\')
+    console.log('               https://api.featureninjas.com/flags/<account-name>/<repo-name>/<branch_ref_name> \\')
+    console.log('               -H \'X-FeatureNinjas-Secret: <secret>\'')
+    console.log('')
+
   } catch (error) {
     log.error(error.message)
   }
